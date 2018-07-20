a60f79cd960fc896270bd7bb0c1c9d1832d93a2e
jaja2017 <m8r-xvl6av@mailinator.com>
2018-07-20 13:14:48 +0200

    Undo Button mit Schutz vor leerem Stack

--- CmdLinePro/src/de/jaan/cmdtst/FXlibProjController.java
+++ CmdLinePro/src/de/jaan/cmdtst/FXlibProjController.java
@@ -1,19 +1,28 @@
 package de.jaan.cmdtst;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Stack;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.text.Text;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
+import javafx.scene.input.InputMethodEvent;
 
-public class FXlibProjController {
+public class FXlibProjController implements Initializable {
 
 	@FXML TextField nameField;
 	@FXML PasswordField passwordField;
 	@FXML Text ausgabe;
+	Stack<ObjectString> stack;
 
 	public FXlibProjController() {
-		
+		this.stack= new Stack<ObjectString>();
 	}
 
 	@FXML public void handleSubmitButtonAction(ActionEvent event) {
@@ -23,4 +32,34 @@ public class FXlibProjController {
 		ausgabe.setText( Main.anz + " * " + Main.text);
 	}
 
+
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		nameField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+    		public void changed(final ObservableValue<? extends String> observable, final String oldValue, final String newValue) {
+    	        // this will run whenever text is changed
+    			ausgabe.setText(oldValue + "-->" + newValue);
+    			stack.push(new ObjectString(nameField,oldValue));
+    	    }
+        });
+		passwordField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+    		public void changed(final ObservableValue<? extends String> observable, final String oldValue, final String newValue) {
+    	        // this will run whenever text is changed
+    			ausgabe.setText(oldValue + "-->" + newValue);
+    			stack.push(new ObjectString(passwordField,oldValue));
+    	    }
+        });
+	}
+
+	@FXML public void handleUndoButtonAction(ActionEvent event) {
+		ObjectString feldStr;
+		if(! stack.isEmpty()) {
+			feldStr= stack.pop();
+			feldStr.obj.setText(feldStr.str);
+			stack.pop();
+		}
+	}
 }
