d1e0f88945aa9443793f5503dfc9e3c3082085f3
jaja2017 <m8r-xvl6av@mailinator.com>
2018-07-20 12:29:46 +0200

    Undo Stack und Button

---
+++ CmdLinePro/src/de/jaan/cmdtst/ObjectString.java
@@ -0,0 +1,14 @@
+package de.jaan.cmdtst;
+
+import javafx.scene.control.TextField;
+
+public class ObjectString {
+	TextField obj;
+	String str;
+
+	public ObjectString(TextField obj, String str) {
+		this.obj=obj;
+		this.str=str;
+	}
+
+}
