1737aa161b26fb9a59f71a8cc603a9df0657f2e2
J-a-n-R <31163111+J-a-n-R@users.noreply.github.com>
2018-07-24 21:43:44 +0200

    StdOut StdErr nach TextArea console ausgeben

--- CmdLinePro/src/de/jaan/cmdtst/FXlibProjController.java
+++ CmdLinePro/src/de/jaan/cmdtst/FXlibProjController.java
@@ -1,9 +1,14 @@
 package de.jaan.cmdtst;
 
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Stack;
 
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -12,15 +17,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.text.Text;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
-import javafx.scene.input.InputMethodEvent;
+import javafx.scene.control.TextArea;
 
 public class FXlibProjController implements Initializable {
 
 	@FXML TextField nameField;
 	@FXML PasswordField passwordField;
 	@FXML Text ausgabe;
+	@FXML TextArea console;
+	private PrintStream ps = new PrintStream(new Console(console));
 	Stack<ObjectString> stack;
-
+	
 	public FXlibProjController() {
 		this.stack= new Stack<ObjectString>();
 	}
@@ -32,10 +39,27 @@ public class FXlibProjController implements Initializable {
 		ausgabe.setText( Main.anz + " * " + Main.text);
 	}
 
+  public class Console extends OutputStream {
+    private TextArea console;
 
+    public Console(TextArea console) {
+        this.console = console;
+    }
+
+    public void appendText(String valueOf) {
+        Platform.runLater(() -> console.appendText(valueOf));
+    }
+
+    public void write(int b) throws IOException {
+        appendText(String.valueOf((char)b));
+    }
+  }
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		ps = new PrintStream(new Console(console)) ;
+    System.setOut(ps);
+    System.setErr(ps);
 		nameField.textProperty().addListener(new ChangeListener<String>() {
             @Override
     		public void changed(final ObservableValue<? extends String> observable, final String oldValue, final String newValue) {
@@ -49,15 +73,17 @@ public class FXlibProjController implements Initializable {
     		public void changed(final ObservableValue<? extends String> observable, final String oldValue, final String newValue) {
     	        // this will run whenever text is changed
     			ausgabe.setText(oldValue + "-->" + newValue);
+    			System.out.println(newValue);
     			stack.push(new ObjectString(passwordField,oldValue));
     	    }
         });
+		
 	}
 
 	@FXML public void handleUndoButtonAction(ActionEvent event) {
 		ObjectString feldStr;
 		if(stack.isEmpty()) {
-			ausgabe.setText("Änderungsspeicher leer");
+			ausgabe.setText("ï¿½nderungsspeicher leer");
 		} else {
 			feldStr= stack.pop();
 			feldStr.obj.setText(feldStr.str);
